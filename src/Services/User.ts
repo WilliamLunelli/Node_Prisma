bd81e87f40fdd9fd45757618b062e60b4f63d265
williamlunelli <williamlunelli07@gmail.com>
2024-08-28 08:52:25 -0300

    fix

--- src/Services/User.ts
+++ src/Services/User.ts
@@ -47,3 +47,41 @@ export const getUserByEmail = async (email: string) => {
   });
   return user;
 };
+
+export const getPostById = async (id: number) => {
+  const post = await prisma.post.findUnique({
+    where: { id },
+  });
+  return post;
+};
+
+export const createPost = async (data: Prisma.PostUncheckedCreateInput) => {
+  try {
+    const post = await prisma.post.create({ data });
+    return post;
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P1002") {
+        return {
+          error: "Algo de errado aconteceu, tente novamente mais tarde",
+        };
+      }
+    }
+  }
+};
+
+export const createPosts = async (posts: Prisma.PostUncheckedCreateInput[]) => {
+  try {
+    return await prisma.post.createMany({
+      data: posts,
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        return { error: "Email already exists" };
+      }
+    }
+
+    console.error(error);
+  }
+};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { prisma } from "../Libs/prisma";
-import { createUser, createUsers, getAllUsers, getUserByEmail } from "../Services/User";
+import { createPost, createPosts, createUser, createUsers, getAllUsers, getPostById, getUserByEmail } from "../Services/User";
 import { count, error } from "console";
+import { title } from "process";
 
 const router = Router();
 
@@ -30,6 +31,7 @@ router.post("/user", async (req, res) => {
   res.status(201).json({ user });
 });
 
+
 router.post("/users", async (req, res) => {
   const result = await createUsers([
     { name: "will", email: "jeferçon@gmail.com" },
@@ -48,3 +50,27 @@ router.get("/user", async(req, res) => {
   const result = await getUserByEmail('william@gmail.com');
   res.json(result);
 })
+
+router.get("/post", async(req, res) =>{
+  const result = await getPostById(1)
+});
+
+router.post("/post", async(req,res)=>{
+  const result = await createPost({
+    userID: 1,
+    title: "testezinho de post",
+    body: "corpo de post",
+  });
+
+  res.json(result)
+})
+
+
+router.post("/posts", async (req, res) => {
+  const result = await createPosts([
+    {userID:1, title: "teste denovo", body: "pao de batata e massa"},
+    {userID:1, title: "teste denovo", body: "pao de batata e massa"},
+    {userID:1, title: "teste denovo", body: "pao de batata e massa"}
+  ]);
+  res.json(result);
+});
